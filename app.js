38c4d5161c26200e8a23003f3ed8ae36cdced905
Alan <fadhlannazhif5@gmail.com>
2023-11-08 19:23:47 +0700

    add cors

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose");
+var cors = require('cors');
 
-const User = require("./models/Pengguna");
+const Pengguna = require("./models/Pengguna");
+
 
 var app = express();
+app.use(cors());
 
 // ! view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +26,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // ! DATABASE CONNECTION
-const DbURI = "mongodb+srv://Dzikri:<password>@cluster0.yimmfp3.mongodb.net/?retryWrites=true&w=majority/GymPal"
+const DbURI = "mongodb+srv://GymPal:Gwencana@cluster0.yimmfp3.mongodb.net/GymBal"
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(DbURI, options)
   .then((result) => {
-    app.listen(3000, () => {
-      console.log("listening on port 3000...");
+    app.listen(3001, () => {
+      console.log("listening on port 3001...");
     })
   })
   .catch((err) => console.log(err));
