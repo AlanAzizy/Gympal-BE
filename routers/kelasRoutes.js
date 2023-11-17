f45b35bf06cb7011852eaffb62edd494561c68bb
Alan <18221128@std.stei.itb.ac.id>
2023-11-17 17:42:56 +0700

    mendaftar dan menghapus kelas oleh anggota

---
+++ routers/kelasRoutes.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+const kelasController = require("../controllers/kelasController");
+
+router.put("/mendaftarKelas",kelasController.mendaftarKelas);
+router.put("/menghapusKelas",kelasController.menghapusKelas)
+module.exports = router;
