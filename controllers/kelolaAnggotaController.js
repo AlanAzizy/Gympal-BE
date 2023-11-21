229339062d6bee7bef858962e663e41dc52bc0a9
gevyndo <18221076@std.stei.itb.ac.id>
2023-11-21 23:10:55 +0700

    baru bikin get all data belum dites

--- controllers/kelolaAnggotaController.js
+++ controllers/kelolaAnggotaController.js
@@ -1,4 +1,5 @@
 const Anggota = require("../models/Anggota");
+const Pembayaran = require("../models/Pembayaran");
 
 module.exports.setActive = async (req, res) => {
     try {
@@ -22,4 +23,35 @@ module.exports.setNonActive = async (req, res) => {
         console.log(err);
         res.status(400).json({ error: err });
     }
+}
+
+// butuh endpoint get all data anggota
+module.exports.getAllDataPengguna = async (req, res) => {
+    try{
+        const anggota = await Anggota.find();
+        for (satu_anggota of anggota){
+            last_index = satu_anggota.kumpulanPembayaran.length-1;
+            if (last_index>0){
+                anggota.last_payment = await Pembayaran.findOne({_id : satu_anggota.kumpulanPembayaran[last_index]._id});
+            }else{
+                anggota.last_payment = {};
+            }
+        }
+        const millisecondsInMonth = 30 * 24 * 60 * 60 * 1000;
+        for (satu_anggota of anggota){
+            if (satu_anggota.last_payment.statusPembayaran && (new Date() - anggota.tanggalPembayaran > millisecondsInMonth)){
+                satu_anggota.payment = true;
+                
+            }else{
+                satu_anggota.payment = false;
+            }
+
+            satu_anggota.expdate = new Date(anggota.tanggalPembayaran+millisecondsInMonth);
+        }
+        res.status(201).json(anggota);
+
+    }catch(err){
+        res.status(400).json(err.message);
+    }
+
 }
