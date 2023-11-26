37dd2d69bb0ca3c5787ae734c9843fc6faf711fa
gevyndo <18221076@std.stei.itb.ac.id>
2023-11-26 16:07:56 +0700

    Update authMiddlewares.js

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -52,7 +52,7 @@ module.exports.authCheck = (req, res, next) => {
 
 module.exports.protectRoute = (req, res, next) => {
     // TODO ambil token dari cookies
-    const token = req.cookies.jwt;
+    const token = req.headers.cookies;
     // TODO cek jwtnya ada ato enggak
     if (jwt) {
         //cek apaka masuk
@@ -86,7 +86,7 @@ module.exports.protectRoute = (req, res, next) => {
 
 module.exports.adminAuthorization = (req, res, next) => {
     // TODO ambil dulu tokennya, dan lakukan decode terhadap tokennya
-    const token = req.cookies.jwt;
+    const token = req.headers.cookies;
     if (token) {
         jwt.verify(token, "9cdef41de4e4016adb9d8bascbsaocjbasovbaowq9071291179", async (err, decodedToken) => {
             // TODO ambil id penggunanya
